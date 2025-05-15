438633ba71cc45b71948733a0195d9617ff45c84
UE <unrealempowerment@gmail.com>
2025-05-15 11:23:27 -0300

    Add python docs

--- client/python/projectairsim/docs/conf.py
+++ client/python/projectairsim/docs/conf.py
@@ -19,5 +19,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'haiku'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'

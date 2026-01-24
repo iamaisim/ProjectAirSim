ea102537d9f50327234b6b46dd92e0024642d1b6
wjy <jia2jiayuan@163.com>
2026-01-24 11:25:02 +0800

    Inform the user that the environment variable UE_ROOT is not set.

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@
 # MIT License.
 
 set -e
+
+# Inform the user that the environment variable UE_ROOT is not set.
+if [ -z "$UE_ROOT" ]; then
+    echo "The UE_ROOT environment variable is not set. Please set it first by executing the command: export UE_ROOT=/path/to/UnrealEngine" >&2
+    exit 1
+fi
+
 make -f build_linux.mk $1
--- setup_linux_dev_tools.sh
+++ setup_linux_dev_tools.sh
@@ -5,6 +5,12 @@
 
 set -e
 
+# Inform the user that the environment variable UE_ROOT is not set.
+if [ -z "$UE_ROOT" ]; then
+    echo "The UE_ROOT environment variable is not set. Please set it first by executing the command: export UE_ROOT=/path/to/UnrealEngine" >&2
+    exit 1
+fi
+
 sudo apt-get update
 
 # Install lsb_release to check Ubuntu version

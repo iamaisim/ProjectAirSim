9eb4337a9e66df4e91a53df49db0e908158819a1
jonyMarino <unrealempowerment@gmail.com>
2026-08-25 12:49:00 -0300

    Use supported MSVC toolset for Unreal 5.7

--- build.cmd
+++ build.cmd
@@ -59,7 +59,7 @@ REM =====================================================
 if "%UE_MINOR%"=="2" (
   set "MSVC_VER=14.37"
 ) else if "%UE_MINOR%"=="7" (
-  set "MSVC_VER=14.39"
+  set "MSVC_VER=14.44"
 ) else (
   echo:
   echo Unsupported Unreal Engine version 5.%UE_MINOR%

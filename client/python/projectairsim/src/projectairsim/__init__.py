5fb2abeac83c8c83cb570393931592aab318724d
Jonathan <jony@iamaisim.com>
2026-09-07 13:54:11 -0300

    Prepare Project AirSim v1.0.1 release

--- client/python/projectairsim/src/projectairsim/__init__.py
+++ client/python/projectairsim/src/projectairsim/__init__.py
@@ -12,7 +12,7 @@ from .env_actor import EnvActor
 from .static_sensor_actor import StaticSensorActor
 from .unreal_vehicle import UnrealVehicle
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     "Drone",
